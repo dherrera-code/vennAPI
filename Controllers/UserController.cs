44543104d43db82c8ac819f7b70d7a61573580c5
dherrera-code <dherr929@gmail.com>
2026-03-27 10:50:31 -0700

    Created Endpoint to update user's profile by userId!

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -74,13 +74,16 @@ namespace vennAPI.Controllers
             return BadRequest(new {Message = "Updating Username Failed! New Username may already be created!"});
         }
 
-        [HttpGet("GetUserProfileById/{id}")]
+        [HttpGet("GetProfileByUserId/{id}")]
         public async Task<ActionResult<ProfileDTO>> GetUserProfileByUserId(int id)
         {
             return await _userService.GetUserProfileByIdAsync(id);
         }
 
-        
-        
+        [HttpPut("UpdateUserProfileByUserId/{id}")]
+        public async Task<ActionResult<ProfileDTO>> UpdateUserProfileByUserId(int id, ProfileDTO profileInfo)
+        {
+            return await _userService.UpdateProfileInfoByUserIdAsync(id, profileInfo);
+        }
     }
 }
--- Services/UserServices.cs
+++ Services/UserServices.cs
@@ -27,11 +27,11 @@ namespace vennAPI.Services
         }
         public async Task<bool> CreateAccount(CreateAccountDTO newUser)
         {
-            if(await DoesUserExistEmail(newUser.Email)) return false;
+            if (await DoesUserExistEmail(newUser.Email)) return false;
 
-            if(await DoesUsernameExist(newUser.Username)) return false;
+            if (await DoesUsernameExist(newUser.Username)) return false;
 
-            UserModel createUser =  new();
+            UserModel createUser = new();
             PasswordDTO EncryptedPassword = HashPassword(newUser.Password);
             createUser.Username = newUser.Username;
             createUser.Email = newUser.Email;
@@ -51,22 +51,22 @@ namespace vennAPI.Services
         {
             UserModel currentUser = await GetUserInfoByUsernameAsync(userLogin.Username);
 
-            if(currentUser == null) await GetUserByEmailAsync(userLogin.Username);
+            if (currentUser == null) await GetUserByEmailAsync(userLogin.Username);
 
-            if(currentUser == null) return null;
+            if (currentUser == null) return null;
 
-            if(!VerifyPassword(userLogin.Password, currentUser.Salt, currentUser.Hash)) return null;
+            if (!VerifyPassword(userLogin.Password, currentUser.Salt, currentUser.Hash)) return null;
 
             return GenerateJWT(new List<Claim>());
 
         }
-        
+
         // ? Below are Helper Functions for User Services!
 
         //function to generate JWT claim token!
         private string GenerateJWT(List<Claim> claims)
         {
-             var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"])); // gives us access to appsettings.json where the secret key is stored!
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"])); // gives us access to appsettings.json where the secret key is stored!
             var SigningCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
             var tokenOptions = new JwtSecurityToken( //our token is now valid for our web app
@@ -85,7 +85,7 @@ namespace vennAPI.Services
         {
             byte[] saltByte = Convert.FromBase64String(salt);
             string checkHash;
-            using(var derivedBytes = new Rfc2898DeriveBytes(password, saltByte, 310000, HashAlgorithmName.SHA256))
+            using (var derivedBytes = new Rfc2898DeriveBytes(password, saltByte, 310000, HashAlgorithmName.SHA256))
             {
                 checkHash = Convert.ToBase64String(derivedBytes.GetBytes(32));
                 return hash == checkHash;
@@ -101,12 +101,12 @@ namespace vennAPI.Services
         private async Task<bool> DoesUserExistEmail(string email) => await _dataContext.Users.SingleOrDefaultAsync(user => user.Email == email) != null;
 
         private async Task<bool> DoesUsernameExist(string username) => await _dataContext.Users.SingleOrDefaultAsync(user => user.Username == username) != null;
-        
+
         // Helper function to HashPassword!
         private static PasswordDTO HashPassword(string password)
         {
             byte[] SaltBytes = RandomNumberGenerator.GetBytes(64);
-            
+
             string salt = Convert.ToBase64String(SaltBytes);
             string hash;
 
@@ -130,7 +130,7 @@ namespace vennAPI.Services
             // user.Username = currentUser.Username;
             // user.Email = currentUser.Email;
             // return user;
-            if(currentUser == null) return null;
+            if (currentUser == null) return null;
             return currentUser;
         }
 
@@ -138,7 +138,7 @@ namespace vennAPI.Services
         public async Task<bool> DeleteUser(string userToRemove)
         {
             var user = await GetUserInfoByUsernameAsync(userToRemove);
-            if(user == null) return false;
+            if (user == null) return false;
 
             _dataContext.Users.Remove(user);
             return await _dataContext.SaveChangesAsync() != 0;
@@ -147,11 +147,11 @@ namespace vennAPI.Services
         public async Task<bool> EditUsername(int id, string newUsername)
         {
             //If new username already exist, then return false!
-            if(await DoesUsernameExist(newUsername)) return false;
+            if (await DoesUsernameExist(newUsername)) return false;
 
             var findUser = await GetUserInfoByUserIdAsync(id);
             //If user to update isn't found return false
-            if(findUser == null) return false;
+            if (findUser == null) return false;
 
             findUser.Username = newUsername;
             _dataContext.Users.Update(findUser);
@@ -174,8 +174,8 @@ namespace vennAPI.Services
         public async Task<ActionResult<ProfileDTO>> GetUserProfileByIdAsync(int id)
         {
             var user = await GetUserByUserIdAsync(id);
-            
-            if(user.Value == null) throw new Exception();
+
+            if (user.Value == null) throw new Exception();
             ProfileDTO userProfile = new();
             userProfile.Username = user.Value.Username;
             userProfile.Email = user.Value.Email;
@@ -184,6 +184,39 @@ namespace vennAPI.Services
             userProfile.Banner = user.Value.Banner;
 
             return userProfile;
+        }
+
+        public async Task<ActionResult<ProfileDTO>> UpdateProfileInfoByUserIdAsync(int id, ProfileDTO profileInfo)
+        {
+            var user = await GetUserByUserIdAsync(id);
+            // lets update the userInfo
+            if (user.Value == null) throw new Exception();
+
+            // the nested if statement will throw an error if username exist and its not the current username!
+            if (await DoesUsernameExist(profileInfo.Username))
+            {
+                if (profileInfo.Username != user.Value.Username)
+                { // checks if username
+                    throw new InvalidDataException();
+                }
+            }
+            if (await DoesUserExistEmail(profileInfo.Email))
+            {
+                if (profileInfo.Email != user.Value.Email)
+                { // checks if username
+                    throw new InvalidDataException();
+                }
+            }
+            user.Value.Username = profileInfo.Username;
+            user.Value.Email = profileInfo.Email;
+            user.Value.Description = profileInfo.Description;
+            user.Value.UserIcon = profileInfo.UserIcon;
+            user.Value.Banner = profileInfo.Banner;
+            _dataContext.Update(user.Value);
+            await _dataContext.SaveChangesAsync();
+            //update user then save changes!
+
+            return await GetUserProfileByIdAsync(id);
         }
     }
 }
